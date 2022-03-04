723b7609d88b52c29ec388a25f838fe6538c5101
Ahmad Hafeez <ahmmad26@gmail.com>
2022-03-04 11:54:59 +0500

    Removed unused types

--- src/services/messages/mesages.types.ts
+++ src/services/messages/mesages.types.ts
@@ -14,44 +14,10 @@ export interface message {
   pageInfo: pageInfo;
 }
 
-export interface responseData {
-  min: number;
-  max: number;
-  avg: string;
-}
-
-export type numberResponseNode = {
-  number: responseData;
-};
-
-export type responseTimeResponseNode = {
-  responseTimes: responseData;
-};
-
-export type wordsResponseNode = {
-  words: responseData;
-};
-
-export type emojiResponseNode = {
-  emojis: responseData;
-};
-
 export type channel = {
   messages: message;
 };
 
 export type channelResponse = {
   channel: channel;
-};
-
-export type statusResponse = {
-  messages: number;
-  queries: number;
-  responseTime: responseData;
-  characters: responseData;
-  words: responseData;
-  numbers: responseData;
-  emojis?: responseData;
-  verbs?: responseData;
-  noun?: responseData;
 };
